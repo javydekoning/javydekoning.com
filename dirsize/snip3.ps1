f17703b11eccea43d0e7f9f234d0b96e3c43e061
Javy de Koning <jdekoning@ipsoft.com>
2016-05-26 21:04:22 +0000

    Snip3.ps1

---
+++ dirsize/snip3.ps1
@@ -0,0 +1,23 @@
+﻿PS C:\Users\jdekoning\AppData\Local\Application Data> cmd /c dir /A:H
+Volume in drive C has no label.
+Volume Serial Number is E653-EF16
+
+Directory of C:\Users\jdekoning\AppData\Local\Application Data
+
+File Not Found
+
+PS C:\Users\jdekoning\AppData\Local\Application Data> Enter-PSSession -ComputerName localhost
+
+[localhost]: PS C:\Users\jdekoning\AppData\Local\Application Data> cmd /c dir /A:H
+Volume in drive C has no label.
+Volume Serial Number is E653-EF16
+
+Directory of C:\Users\jdekoning\AppData\Local\Application Data
+
+05/04/2016  10:47 AM    <JUNCTION>     Application Data [C:\Users\jdekoning\AppData\Local]
+05/04/2016  10:47 AM    <JUNCTION>     History [C:\Users\jdekoning\AppData\Local\Microsoft\Windows\History]
+05/19/2016  08:34 AM            23,699 IconCache.db
+05/04/2016  10:47 AM    <JUNCTION>     Temporary Internet Files [C:\Users\jdekoning\AppData\Local\Microsoft\Windows\INet
+Cache]
+               1 File(s)         23,699 bytes
+               3 Dir(s)  127,835,623,424 bytes free

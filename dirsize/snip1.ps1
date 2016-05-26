db2b133f3aa5c75cb7ed5aa42eb7c2fbd4ae7efb
Javy de Koning <jdekoning@ipsoft.com>
2016-05-26 20:53:13 +0000

    snippet 1

---
+++ dirsize/snip1.ps1
@@ -0,0 +1,12 @@
+﻿#Use erroraction silentlycontinue to ignore access denied errors 
+(Get-ChildItem $home -Recurse -force -erroraction SilentlyContinue | Measure-Object length -Sum).sum 
+
+#Good old Dir, recursively
+((cmd /c dir $home /-C /S /A:-D-L)[-2] -split '\s+')[3]
+
+#RoboCopy in list only mode: 
+(robocopy.exe $home c:\fakepathduh /L /XJ /R:0 /W:1 /NP /E /BYTES /NFL /NDL /NJH /MT:64)[-4] -replace '\D+(\d+).*','$1'
+
+17996994274
+17996994274
+17995815085
